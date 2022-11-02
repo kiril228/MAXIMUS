359dc176d67931c8b7bd803a9baecf282766b77a
Kirill Nizhnik <kiril.niznik@gmail.com>
2022-11-03 01:31:03 +0200

    update html and connection in database, new Java Class

--- src/main/java/com/mycompany/maximus/config/SecurityConfig.java
+++ src/main/java/com/mycompany/maximus/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.mycompany.maximus.config;
 import com.mycompany.maximus.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
